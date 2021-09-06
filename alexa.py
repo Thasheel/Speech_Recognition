0f2f83b2b338380cd245e5f6c3d825991ac9c349
Mohammed Thasheel <spidy9741@gmail.com>
2021-09-06 10:31:11 +0530

    new line added

---
+++ alexa.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+
+listener = sr.Recognizer()
+try:
+    with sr.Microphone() as source:
+            print('listening.....')
+            voice=listener.listen(source)
+            command=listener.recognize_google(voice)
+            print(command)
+except:
+    pass
